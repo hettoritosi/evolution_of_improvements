f733a7e3e9d28931d019a94b75e0a0c77bb38a17
Hettori <hettori@gmail.com>
2015-07-07 10:42:14 -0300

    Layout home done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
-module ApplicationHelper
-end
+  module ApplicationHelper
+    # Returns the full title on a per-page basis.       # Documentation comment
+    def full_title(page_title = '')                     # Method def, optional arg
+      base_title = "Checkout Improvements Studiare"  # Variable assignment
+      if page_title.empty?                              # Boolean test
+        base_title                                      # Implicit return
+      else
+        page_title + " | " + base_title                 # String concatenation
+      end
+    end
+  end
