bdc9db88b63b3b23651b2f3ea24d510ddcb6eba6
Hettori <hettori@gmail.com>
2015-07-09 16:31:23 -0300

    remove duplication

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   has_many :comments
