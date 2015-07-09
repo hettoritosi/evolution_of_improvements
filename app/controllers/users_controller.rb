5ab3e6b5db14ba71c7d1ba40ab4c4a54e1adca8b
Hettori <hettori@gmail.com>
2015-07-09 16:35:56 -0300

    APP done 1.0.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :only_logged, except: [:create]
+  before_action :only_logged, except: [:create,:new]
   # GET /users
   # GET /users.json
   def index
