b29c295d4a974c71a7fa9ad117dfff5f01d4adea
Hettori <hettori@gmail.com>
2015-10-30 18:00:21 -0200

    Heroku CSV IMPORT WORKS2

--- app/controllers/import_logs_controller.rb
+++ app/controllers/import_logs_controller.rb
@@ -5,12 +5,35 @@ class ImportLogsController < ApplicationController
     @import_log.status_import = "In Progress"
     @import_log.save
     current_line = 1
+    if @import_log.file.blank?
+    else
     csv_file = CSV.read(@import_log.file.path)
-    ImportLogsWorker.perform_async(@import_log.id, csv_file)
+    total = csv_file.count
+    csv_file.each do |row|
+      record = Improvement.new(
+          :title => (row[0].blank? ? 'Title missing' : row[0]),
+          :content   => row[1],
+          :category  => row[3],
+          :status_id => '2',            #id 2 = In Progress
+          :user_id => '1',              #id 6 = Murilo
+          :responsible_id =>'1'
+      )
+      record.save!
+      total_percent = (100*current_line)/total
+      current_line += 1
+
+      if @import_log.total_percent != total_percent
+        @import_log.total_percent = total_percent
+        @import_log.save
+      end
+    end
+    @import_log.status_import = "Finished"
+    end
     respond_to do |format|
       if @import_log.save
         format.html { redirect_to controller: 'import_logs', action: 'new', id: @import_log.id}
       else
+        @import_type = [['Tasks', "Improvement"]]
         format.html { render :new }
       end
     end
