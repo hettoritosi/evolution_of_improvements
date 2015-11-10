703d7ab4aa86be2353caf843c441258d213f94e3
Hettori <hettori@gmail.com>
2015-11-10 11:03:46 -0200

    CSV Upload Works

--- app/controllers/import_logs_controller.rb
+++ app/controllers/import_logs_controller.rb
@@ -1,19 +1,44 @@
 class ImportLogsController < ApplicationController
 
-
+  require 'csv'
 
   def create
     @import_log = ImportLog.new(importlog_params)
-    @import_log.status_import = "Initialized"
+    @import_log.status_import = "In Progress"
     @import_log.save
     if @import_log.file.blank?
     else
-    user = current_user.id
-    ImportLogsWorker.perform_async(@import_log.id,user)
+      user = current_user.id
+      current_line = 2
+      total = CSV.read(@import_log.file.path).count
+
+
+      CSV.foreach(@import_log.file.path, :headers => true, :col_sep => ',') do |row|
+        Improvement.create( {
+                                :title => row['Macro'] || 'Título Faltando',
+                                :content   => row['Frente'],
+                                :category  => row['Projeto'],
+                                :status_id => '2',         #id 2 = In Progress
+                                :user_id => user,
+                                :responsible_id => user
+                            }
+        )
+        total_percent = (100*current_line)/total
+        current_line += 1
+        if @import_log.total_percent != total_percent
+          @import_log.total_percent = total_percent
+          @import_log.save
+        end
+      end
+      @import_log.status_import = "Finished"
+      @import_log.save
     end
     respond_to do |format|
       if @import_log.save
         format.html { redirect_to controller: 'import_logs', action: 'new', id: @import_log.id}
+        if @import_log.total_percent == 100
+        flash[:notice] = "CSV was successfully imported."
+          end
       else
         @import_type = [['Tasks', "Improvement"]]
         format.html { render :new }
