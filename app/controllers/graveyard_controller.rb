1e27f8588b7562e0834818b1c0f38d673480535c
Hettori <hettori@gmail.com>
2015-11-19 15:14:58 -0200

    Implementacao do Cemiterio

--- app/controllers/graveyard_controller.rb
+++ app/controllers/graveyard_controller.rb
@@ -7,7 +7,7 @@ class GraveyardController < ApplicationController
   def index
     @user = User.find(current_user)
     all_status = Status.get_all_status
-    @improvements = Improvement.search(params[:search]).where("status_id == 3", current_user.id)
+    @improvements = Improvement.search(params[:search]).where("status_id = 3", current_user.id)
                         .where("status_id in (?)", params[:status] || all_status)
                         .order(sort_column + " " + sort_direction)
                         .paginate(:per_page => 10, :page => params[:page])
